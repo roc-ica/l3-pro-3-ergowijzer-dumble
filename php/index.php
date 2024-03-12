777091999fed4a746c5185fc178a4c73de9e06fb
Colin Poort <581827@edu.rocmn.nl>
2024-03-12 12:24:00 +0100

    added php

---
+++ php/index.php
@@ -0,0 +1,16 @@
+<!-- Er hoeft geen PHP in de website. -->"
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link href = "css/style.css" rel = "stylesheet">   
+ <script src="js/main.js"></script>
+    <title>Ergowijzer</title>
+</head>
+<body>
+    <?php
+
+    ?>
+</body>
+</html>
