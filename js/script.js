cece7b0d00b7c658f2047a4e79433f52b18d1637
Rouzbeh Sayadifar <Rouzbeh.sayadifar@gmail.com>
2024-04-03 09:10:59 +0200

    begin video player

--- js/script.js
+++ js/script.js
@@ -16,3 +16,34 @@ gsap.to("#rect", {
     alignOrigin: [0.5, 0.5]
   }
 });
+
+
+
+// video player
+
+// Get the video element and each link
+const video = document.getElementById("my-video");
+const link1 = document.getElementById("video-link-1");
+const link2 = document.getElementById("video-link-2");
+const link3 = document.getElementById("video-link-3");
+
+// Define the URLs of the videos you want to play
+const videoUrl1 = "";
+const videoUrl2 = "https://example.com/video2.mp4";
+const videoUrl3 = "https://example.com/video3.mp4";
+
+// Add an event listener to each link that changes the video source when clicked
+link1.addEventListener("click", () => {
+  video.src = videoUrl1;
+  video.play();
+});
+
+link2.addEventListener("click", () => {
+  video.src = videoUrl2;
+  video.play();
+});
+
+link3.addEventListener("click", () => {
+  video.src = videoUrl3;
+  video.play();
+});
